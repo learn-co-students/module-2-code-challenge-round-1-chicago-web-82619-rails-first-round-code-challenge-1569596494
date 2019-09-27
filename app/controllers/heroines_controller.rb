b3f0d8fb5da7c372bf63def012bf82b21103aa0a
justrmarks <blackbirdwanderer@gmail.com>
2019-09-27 10:42:54 -0500

    initial solution

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    heroine = Heroine.new(heroine_params)
+    if heroine.save
+      flash[:message] = "New Heroine Created"
+      redirect_to heroine_path(heroine)
+    else
+      flash[:message] = heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:super_name,:name,:power_id)
+  end
 end
