22d37398d5890ca1e7a634c80e6b6f8fb74563d5
github username <github email address>
2019-09-27 11:15:07 -0500

    made buttons big yo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  resources :powers, only: [:index, :show, :new, :create]
+  resources :powers, only: [:index, :show]
   resources :heroines, only: [:index, :show, :new, :create]  
 end
